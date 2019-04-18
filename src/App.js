463a4abfa25efe6cad55a64ca9b8e0108cc6ce2c
stefhangibson <stefhangibson@icloud.com>
2019-04-18 13:24:22 -0500

    sign in

--- src/App.js
+++ src/App.js
@@ -32,7 +32,8 @@ class App extends Component {
     this.state ={
       input: '',
       imageUrl:'',
-      box: {}
+      box: {} ,
+      route: 'signin'
     }
   }
 
@@ -67,18 +68,26 @@ displayFaceBox = (box) => {
     .catch(err => console.log(err));
 }
 
+onRouteChange = (route) => {
+  this.setState({route: route});
+}
+
   render() {
     return (
       <div className="App">
           <Particles className='particles'
            params ={particleOptions}
           />
-        <Navigation />
-        <SignIn />
+        <Navigation onRouteChange={this.onRouteChange}/>
+        {this.state.route === 'signin'
+        ? <SignIn onRouteChange={this.onRouteChange}/>
+        :<div>
         {/*<Logo />*/}
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
         <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+      </div>
+      }
       </div>
     );
   }
